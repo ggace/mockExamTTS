0e4d9cbfe027707c720761ac8b4008a8739dadc2
ggace <skydream5485@gmail.com>
2021-09-25 08:48:31 +0900

    speak number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import json
 
-year = 2020
+year = 2019
 basicPath = rf"sources/{year}"
 resultPath = rf"output/{year}"
 
@@ -11,7 +11,14 @@ for problem in problems:
     with open(rf'{basicPath}\{problem}.json', 'r', encoding='UTF8') as f:
         json_data = json.load(f)
     content = json_data["content"]
+    
+    tts_number = gTTS(text=f"{year}년 {problem}번", lang='ko')
+    tts_content = gTTS(text=content, lang='en')
+    
+    fileName = rf"{resultPath}\{year}_{problem}.mp3"
 
-    tts = gTTS(text=content, lang='en')
-    tts.save(rf"{resultPath}\{year}_{problem}.mp3")
+    f = open(fileName,'wb') 
+    tts_number.write_to_fp(f)
+    tts_content.write_to_fp(f)
+    f.close()
     
