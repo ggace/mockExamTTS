a80f0b5077dfa97a6a6c5fcb3f524ae7ce9def24
ggace <skydream5485@gmail.com>
2021-09-30 18:40:08 +0900

    마지막에 the end 문구 추가, 모든 연도 변환

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from gtts import gTTS
 import json
 
-year = 2021
-basicPath = rf"sources\{year}"
-resultPath = rf"output\{year}"
+years = [2019, 2020, 2021]
 
-problems = list(range(18, 25))+ list(range(29, 41))
+for year in years:
+    basicPath = rf"sources\{year}"
+    resultPath = rf"output\{year}"
 
-for problem in problems:
-    with open(rf'{basicPath}\{problem}.json', 'r', encoding='UTF8') as f:
-        json_data = json.load(f)
-    content = json_data["content"]
-    
-    tts_number = gTTS(text=f"{year}년 {problem}번", lang='ko')
-    tts_content = gTTS(text=f"{content},", lang='en')
-    
-    fileName = rf"{resultPath}\{year}_{problem}.mp3"
+    problems = list(range(18, 25))+ list(range(29, 41))
 
-    f = open(fileName,'wb') 
-    tts_number.write_to_fp(f)
-    tts_content.write_to_fp(f)
-    f.close()
+    for problem in problems:
+        with open(rf'{basicPath}\{problem}.json', 'r', encoding='UTF8') as f:
+            json_data = json.load(f)
+        content = json_data["content"]
+        
+        tts_number = gTTS(text=f"{year}년 {problem}번", lang='ko')
+        tts_content = gTTS(text=f"{content}. the end.", lang='en')
+        
+        fileName = rf"{resultPath}\{year}_{problem}.mp3"
 
-    print(f"{year} : {problem}")
+        f = open(fileName,'wb') 
+        tts_number.write_to_fp(f)
+        tts_content.write_to_fp(f)
+        f.close()
+
+        print(f"{year} : {problem}")
     
