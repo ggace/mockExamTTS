1bc16f424c728902873c2101aa86528b9f92e048
ggace <skydream5485@gmail.com>
2021-10-01 00:14:01 +0900

    topic 추가

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ for year in years:
         with open(rf'{basicPath}\{problem}.json', 'r', encoding='UTF8') as f:
             json_data = json.load(f)
         content = json_data["content"]
+        topic = json_data["topic"]
         
         tts_number = gTTS(text=f"{year}년 {problem}번", lang='ko')
-        tts_content = gTTS(text=f"{content}. the end.", lang='en')
+        if(topic != ""):
+            tts_topic = gTTS(text=f"주제 : {topic}", lang='ko')
+        tts_content = gTTS(text=content, lang='en')
+        
         
         fileName = rf"{resultPath}\{year}_{problem}.mp3"
 
         f = open(fileName,'wb') 
         tts_number.write_to_fp(f)
+        if(topic != ""):
+            tts_topic.write_to_fp(f)
         tts_content.write_to_fp(f)
         f.close()
 
